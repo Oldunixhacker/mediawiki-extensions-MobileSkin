226644fba7aa9a159873310edae2c85896b03a1e
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-03 12:02:40 +0100

    Update MobileSkin.php

--- includes/MobileSkin.php
+++ includes/MobileSkin.php
@@ -19,19 +19,13 @@
  * @license GPL-2.0-or-later https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-
+$MobileSkinOldSkin = $wgDefaultSkin;
 if ( !isset( $wgNameOfMobileSkin ) ) {
     $wgNameOfMobileSkin = 'minerva';
 }
 if (preg_match("/(mobile|webos|opera mini|android|blackberry|tablet)/i", $_SERVER['HTTP_USER_AGENT'])) {
     $wgDefaultSkin = $wgNameOfMobileSkin;
+    $wgHiddenPrefs[] = 'skin';
+} else {
+    $wgDefaultSkin = $MobileSkinOldSkin;
 }
-$wgHooks['GetPreferences'][] = function( $user, &$preferences ) {
-    if ( preg_match( "/(mobile|webos|opera mini|android|blackberry|tablet)/i", $_SERVER['HTTP_USER_AGENT'] ) ) {
-        $preferences['skin'] = [
-            'type' => 'info',
-            'label-message' => 'mobileskin-no-change',
-        ];
-    }
-    return true;
-};
