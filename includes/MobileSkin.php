827d61409da0a0136ee4314155e490a81f0a1114
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-06-03 11:54:40 +0100

    Add license note
    gpl note, didn't mean to select mit

--- includes/MobileSkin.php
+++ includes/MobileSkin.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * A MediaWiki extension to use another skin on mobile without MobileFrontend
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+ * Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @copyright 2023 TylerMS887
+ * @license GPL-2.0-or-later https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+
 if ( !isset( $wgNameOfMobileSkin ) ) {
     $wgNameOfMobileSkin = 'minerva';
 }
